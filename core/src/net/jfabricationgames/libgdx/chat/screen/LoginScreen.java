2cec324b7cec668dd35faecdc8718c878e64c208
tobias <tobias-fassbender@gmx.de>
2022-05-21 19:22:32 +0200

    added an error label and tested some layout properties

--- core/src/net/jfabricationgames/libgdx/chat/screen/LoginScreen.java
+++ core/src/net/jfabricationgames/libgdx/chat/screen/LoginScreen.java
@@ -2,8 +2,10 @@ package net.jfabricationgames.libgdx.chat.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -18,6 +20,9 @@ public class LoginScreen extends ScreenAdapter {
 	
 	private Stage stage;
 	
+	private TextField textFieldName;
+	private Label labelError;
+	
 	public LoginScreen() {
 		stage = new Stage(new ScreenViewport());
 		Game.getInstance().addInputProcessor(stage);
@@ -27,23 +32,32 @@ public class LoginScreen extends ScreenAdapter {
 	public void show() {
 		Table table = new Table();
 		table.setFillParent(true);
-		//table.setDebug(true);
+		table.setDebug(true);
 		stage.addActor(table);
 		
 		Skin skin = new Skin(Gdx.files.internal("skin/rainbow-ui.json"));
 		
-		TextField textFieldName = new TextField("", skin);
+		textFieldName = new TextField("", skin);
 		TextButton buttonLogin = new TextButton("Login", skin);
+		labelError = new Label("", skin);
+		labelError.getStyle().fontColor = Color.RED;
 		
+		table.add().fillX();
 		table.add(textFieldName).fillX().uniformX();
-		table.row().pad(10, 0, 10, 0);
+		table.add().fillX();
+		table.row().pad(10, 0, 0, 0);
+		table.add().fillX();
 		table.add(buttonLogin).fillX().uniformX();
+		table.add().fillX();
+		table.row().pad(20, 0, 0, 0);
+		table.add(labelError).colspan(3).width(500);
+		labelError.setWrap(true);
 		
 		buttonLogin.addListener(new ChangeListener() {
 			
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
-				Gdx.app.log(getClass().getSimpleName(), "logging in with user name: " + textFieldName.getText());
+				login();
 			}
 		});
 	}
@@ -53,5 +67,16 @@ public class LoginScreen extends ScreenAdapter {
 		ScreenUtils.clear(0f, 0f, 0f, 1f);
 		stage.act(delta);
 		stage.draw();
+	}
+	
+	private void login() {
+		Gdx.app.log(getClass().getSimpleName(), "logging in with user name: " + textFieldName.getText());
+		if (textFieldName.getText().isEmpty()) {
+			labelError.setText("Cannot login with empty name. Please choose a username to login.");
+		}
+		else {
+			labelError.setText("");
+			//TODO change to chat screen
+		}
 	}
 }
